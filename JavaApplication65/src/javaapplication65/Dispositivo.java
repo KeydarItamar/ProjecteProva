ca82b31e683cc199c96022111f581fd83d104665
itamar <itamar@example.com>
2023-09-26 16:30:36 +0200

    creacion de clases

--- JavaApplication65/src/javaapplication65/Dispositivo.java
+++ JavaApplication65/src/javaapplication65/Dispositivo.java
@@ -48,21 +48,5 @@ public class Dispositivo {
     public String toString() {
         return "Dispositivo :" + ", Marca=" + Marca + ", Model=" + Model + "preuBase=" + preuBase;
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        new Dispositivo(100, "Nokia", "365");
-        new Dispositivo(100, "Nokia", "365");
-        new Dispositivo(100, "Nokia", "365");
-        new Dispositivo(100, "Nokia", "365");
-        new Dispositivo(100, "Nokia", "365");
-        
-        SmartPhone s = new SmartPhone("os", "sdfsdfsdf", 111, "nokia", "2011");
-       
- 
-        
-    }
     
 }
